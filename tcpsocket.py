14d1c29fcbf3bb5cbb40e73e160100b82ec03cfe
mamad <ghobadi.mhd@gmail.com>
2016-07-30 00:31:11 +0430

    add and change some comment's

--- tcpsocket.py
+++ tcpsocket.py
@@ -3,27 +3,24 @@ class tcpsocket :
 	'''
 	 make a tcp socket for send and reciving from pop3 server 
 	'''
-	def connect(self , address:str , port:int ) :
+	def connect(self , address:str , port:int ) : # connect to server 
 		self.sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-		self.sock.connect((address , port))
-	def send(self , data):
+		self.sock.connect((address , port)) 
+	def send(self , data): # it send data to socket 
 		self.sock.send(data)
 	def strToByte(string:str ):
 		return string.encode()
 	def recvSingleLine(self):
-		#data = self.sock.recv(1)
 		data = b'' 
 		while True :
-			#array.append(data)
 			byte = self.sock.recv(1) 
 			data = data + byte 
-			#data = self.sock.recv(1)
 			if byte[0] == 13 : # check for CR 
 				byte = self.sock.recv(1) 
 				data = data + byte   
 				if byte[0] == 10 :# check for LF
 					break # we find CR and LF let's get out of loop 
 
-		return data  
+		return data 
 
 
