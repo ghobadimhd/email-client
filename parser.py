3ef67ccb9421fd491973474ff092eb8eb0112baa
mamad <ghobadi.mhd@gmail.com>
2016-08-27 00:50:59 +0430

    pop3parser and list method added

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import re 
 class mail  :
     def __init__(self):
         self.subject = '' 
@@ -17,3 +18,18 @@ class mail  :
         self.deliveredTo = '' 
         self.disposition = '' 
         self.Xmailer = ''
+
+class pop3parser : 
+    '''
+    this class has multiple method for parsing pop3 output 
+    name of method are same as the command of pop3 
+    '''
+    def list(listInput:str):
+        regex = re.compile("([0-9]+) ([0-9]+)")
+        matchList = regex.findall(listInput)
+        print(matchList)
+        # convert string's to number 
+        output = [] 
+        for record  in matchList : 
+            output.append((int(record[0]) , int(record[1]))) 
+        return   output
