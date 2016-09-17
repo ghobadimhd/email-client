c65cf5223710c98fc8f3ce33a19536899ac7a71e
mamad <ghobadi.mhd@gmail.com>
2016-09-17 14:49:45 +0430

    bug fix

--- smtp.py
+++ smtp.py
@@ -2,7 +2,7 @@ import tcpsocket
 class smtp: 
 	def __init__(self):
 		self.ts = tcpsocket.tcpsocket()
-	def conncet(self , ip:str , port:int):
+	def connect(self , ip:str , port:int):
 		self.ts.connect(ip , port) 
 		return self.ts.recvSingleLine()
 	def helo(self, hostname):
