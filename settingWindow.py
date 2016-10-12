01da7d332a9e2e41a830e28f2a38d4673f175802
mamad <ghobadi.mhd@gmail.com>
2016-10-12 22:04:48 +0330

    save and exit button implemented , some bug fixed

--- settingWindow.py
+++ settingWindow.py
@@ -13,24 +13,34 @@ class SettingWindow(QtGui.QMainWindow):
         self.ui = Ui_settingWindow()
         self.ui.setupUi(self)
         self.initialize()
+        self.client_setting = ConfigParser()[setting.CONFIG_SECTION]
 
     def initialize(self):
         setting_file = pathlib.Path(setting.SETTINGS_PATH)
 
         if setting_file.exists() and setting_file.is_file():
-            client_setting = setting.read_section()
-            setting.add_missing_param(client_setting)
+            self.client_setting = setting.read_section()
+            setting.add_missing_param(self.client_setting)
 
-            self.ui.smtpServer_lineEdit.setText(client_setting['smtp_server'])
-            self.ui.smtpUser_lineEdit.setText(client_setting['smtp_user'])
-            self.ui.smtpPassword_lineEdit.setText(client_setting['smtp_pass'])
+            self.ui.smtpServer_lineEdit.setText(self.client_setting['smtp_server'])
+            self.ui.smtpUser_lineEdit.setText(self.client_setting['smtp_user'])
+            self.ui.smtpPassword_lineEdit.setText(self.client_setting['smtp_pass'])
 
-            self.ui.pop3Server_lineEdit.setText(client_setting['pop3_server'])
-            self.ui.pop3User_lineEdit.setText(client_setting['pop3_user'])
-            self.ui.pop3Password_lineEdit.setText(client_setting['pop3_pass'])
+            self.ui.pop3Server_lineEdit.setText(self.client_setting['pop3_server'])
+            self.ui.pop3User_lineEdit.setText(self.client_setting['pop3_user'])
+            self.ui.pop3Password_lineEdit.setText(self.client_setting['pop3_pass'])
 
     def save_button_click(self):
-        pass
+        self.client_setting['smtp_server'] = self.ui.smtpServer_lineEdit.text()
+        self.client_setting['smtp_user'] = self.ui.smtpUser_lineEdit.text()
+        self.client_setting['smtp_pass'] = self.ui.smtpPassword_lineEdit.text()
+
+        self.client_setting['pop3_server'] = self.ui.pop3Server_lineEdit.text()
+        self.client_setting['pop3_user'] = self.ui.pop3User_lineEdit.text()
+        self.client_setting['pop3_pass'] = self.ui.pop3Password_lineEdit.text()
+
+        setting.write(self.client_setting)
+        exit()
 
     def cancel_button_click(self):
         exit()
