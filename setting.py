aa5bcaec6e578903702be4c6f780324dac38a004
mamad <ghobadi.mhd@gmail.com>
2016-10-12 21:41:46 +0330

    bug fix , some new function added

--- setting.py
+++ setting.py
@@ -3,11 +3,18 @@ import  configparser
 SETTINGS_PATH = '.setting'
 CONFIG_SECTION = 'DEFAULT'
 NECESSARY_SETTINGS = ['smtp_server', 'pop3_server', 'pop3_user', 'pop3_pass']
+ALL_SETTINGS = ['smtp_server', 'smtp_user', 'smtp_pass', 'pop3_server', 'pop3_user', 'pop3_pass']
 
 class SettingMissing(Exception):
     """This excepion should used when there is error in setting"""
     def __init__(self, param_name):
         super(SettingMissing, self).__init__()
+        self.param_name = param_name
+
+class SettingEmpty(Exception):
+    """This excepion should used when there is error in setting"""
+    def __init__(self, param_name):
+        super(SettingEmpty, self).__init__()
         self.param_name = param_name
 
 
@@ -41,6 +48,31 @@ def missing_param(setting, param_name):
     if param_name not in setting:
         raise SettingMissing(param_name)
 
-def check_necessary_config(setting, section=CONFIG_SECTION):
-    for param in setting[section]:
+def check_necessary_config(setting):
+    for param in NECESSARY_SETTINGS:
         missing_param(setting, param)
+
+def add_missing_param(setting):
+    """This fuction read setting and if find out some param is missed
+        create it with empty vlaue
+    """
+    for param in ALL_SETTINGS:
+        try:
+            missing_param(setting, param)
+        except SettingMissing:
+            setting[param] = ""
+
+def empty_param(setting, param_name):
+    """Check setting for particular parameter empty
+
+    if parameter is empty in setting file raise an exception
+
+    Args:
+        param_name: name of parameter
+    raise:
+        SettingEmpty:
+
+    """
+    missing_param(setting, param_name)
+    if setting[param_name]:
+        raise SettingEmpty(param_name)
