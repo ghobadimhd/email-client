64aad50d62d2cfc9afe8ab26e7e6fa753e090ac2
mamad <ghobadi.mhd@gmail.com>
2016-10-12 21:42:19 +0330

    initialize method implemented

--- settingWindow.py
+++ settingWindow.py
@@ -1,3 +1,5 @@
+import pathlib
+import setting
 from PyQt4 import QtGui
 from settingWindowUi import Ui_settingWindow
 
@@ -12,7 +14,19 @@ class SettingWindow(QtGui.QMainWindow):
 		self.initialize()
 
 	def initialize(self):
-		pass
+		setting_file = pathlib.Path(setting.SETTINGS_PATH)
+
+		if setting_file.exists() and setting_file.is_file():
+			client_setting = setting.read_section()
+			setting.add_missing_param(client_setting)
+
+			self.ui.smtpServer_lineEdit.setText(client_setting['smtp_server'])
+			self.ui.smtpUser_lineEdit.setText(client_setting['smtp_user'])
+			self.ui.smtpPassword_lineEdit.setText(client_setting['smtp_pass'])
+
+			self.ui.pop3Server_lineEdit.setText(client_setting['pop3_server'])
+			self.ui.pop3User_lineEdit.setText(client_setting['pop3_user'])
+			self.ui.pop3Password_lineEdit.setText(client_setting['pop3_pass'])
 
 	def save_button_click(self):
 		pass
